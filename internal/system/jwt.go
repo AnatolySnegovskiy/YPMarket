6bea69c99fc33f95b7bcd092cfa1755cae3725ed
a.snegovskii <CarrionGrow@Gmail.com>
2024-05-28 21:15:41 +0300

    fix

--- internal/system/jwt.go
+++ internal/system/jwt.go
@@ -10,7 +10,7 @@ func CreateToken(userID uint) (string, error) {
 	var jwtKey = []byte("your_secret_key")
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
-	claims["sub"] = userID
+	claims["user_id"] = userID
 	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 	tokenString, err := token.SignedString(jwtKey)
 	return tokenString, err
@@ -43,7 +43,13 @@ func GetUserID(signedToken string) (int, error) {
 	}
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		userID := int(claims["user_id"].(float64))
+		var userID int
+		if id, ok := claims["user_id"].(float64); ok {
+			userID = int(id)
+		} else {
+			return 0, fmt.Errorf("user_id claim is missing or not of type float64")
+		}
+		
 		return userID, nil
 	}
 
