41627ba83b10d9754c61201723eb92268c95e96e
a.snegovskii <CarrionGrow@Gmail.com>
2024-06-08 18:39:50 +0300

    tests

--- internal/services/server/hendlers/user/user_handlers_test.go
+++ internal/services/server/hendlers/user/user_handlers_test.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"io"
 	"market/internal/services/server/middleware"
 	"net/http"
@@ -35,6 +37,7 @@ func TestHandlers(t *testing.T) {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 	gdb, _ := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+	gdb.Logger = gdb.Logger.LogMode(logger.Silent)
 
 	testCases := []testCase{
 		{
@@ -58,6 +61,40 @@ func TestHandlers(t *testing.T) {
 			requestBody:    WithdrawRequest{Order: "order123", Sum: 100},
 			expectedStatus: http.StatusOK,
 			queryMock:      WithdrawHandlerMockQuery,
+		},
+		{
+			name: "WithdrawHandler - Success no order",
+			handler: http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+				WithdrawHandler(gdb, writer, upLogin(request))
+			}),
+			method:         "POST",
+			url:            "/withdraw",
+			requestBody:    WithdrawRequest{Order: "order123", Sum: 100},
+			expectedStatus: http.StatusOK,
+			queryMock:      WithdrawHandlerMockQueryNoOrder,
+		},
+		{
+			name: "WithdrawHandler - error no order",
+			handler: http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+				WithdrawHandler(gdb, writer, upLogin(request))
+			}),
+			method:         "POST",
+			url:            "/withdraw",
+			requestBody:    WithdrawRequest{Order: "order123", Sum: 100},
+			expectedStatus: http.StatusInternalServerError,
+			queryMock:      WithdrawHandlerMockQueryNoOrderError,
+		},
+		{
+			name: "WithdrawHandler - bad request",
+			handler: http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+				WithdrawHandler(gdb, writer, upLogin(request))
+			}),
+			method:         "POST",
+			url:            "/withdraw",
+			requestBody:    `{"orderws": "order123", "suma": 100}`,
+			expectedStatus: http.StatusBadRequest,
+			queryMock: func(mock sqlmock.Sqlmock) {
+			},
 		},
 		{
 			name: "WithdrawHandler - not enough money",
@@ -84,7 +121,23 @@ func TestHandlers(t *testing.T) {
 			url:            "/withdraw",
 			requestBody:    WithdrawRequest{Order: "order123", Sum: 0},
 			expectedStatus: http.StatusUnprocessableEntity,
-			queryMock:      WithdrawHandlerMockQuery,
+			queryMock: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery(`SELECT \* FROM "users" WHERE "users"."id" = \$1 AND "users"."deleted_at" IS NULL ORDER BY "users"."id" LIMIT \$2`).
+					WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg()).
+					WillReturnRows(sqlmock.NewRows([]string{"id", "created_at", "updated_at", "deleted_at", "email", "password", "balance", "withdrawal"}).
+						AddRow(123, dateMock, dateMock, nil, "nUJ4D@example.com", "password", 0, 0))
+			},
+		},
+		{
+			name: "WithdrawHandler - Over Error",
+			handler: http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+				WithdrawHandler(gdb, writer, upLogin(request))
+			}),
+			method:         "POST",
+			url:            "/withdraw",
+			requestBody:    WithdrawRequest{Order: "order123", Sum: 100},
+			expectedStatus: http.StatusInternalServerError,
+			queryMock:      WithdrawHandlerMockQueryError,
 		},
 	}
 
@@ -99,7 +152,6 @@ func TestHandlers(t *testing.T) {
 			rr := httptest.NewRecorder()
 			tc.queryMock(mock)
 			tc.handler.ServeHTTP(rr, req)
-
 			if assert.Equal(t, tc.expectedStatus, rr.Code) == false {
 				t.Errorf(rr.Body.String())
 			}
@@ -135,6 +187,57 @@ func WithdrawHandlerMockQuery(mock sqlmock.Sqlmock) {
 		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
 		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
 	mock.ExpectCommit()
+}
+
+func WithdrawHandlerMockQueryNoOrder(mock sqlmock.Sqlmock) {
+	mock.ExpectQuery(`SELECT \* FROM "users" WHERE "users"."id" = \$1 AND "users"."deleted_at" IS NULL ORDER BY "users"."id" LIMIT \$2`).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg()).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "created_at", "updated_at", "deleted_at", "email", "password", "balance", "withdrawal"}).
+			AddRow(123, dateMock, dateMock, nil, "nUJ4D@example.com", "password", 5000000, 0))
+	mock.ExpectQuery(`SELECT \* FROM "orders" WHERE number = \$1 AND "orders"."deleted_at" IS NULL ORDER BY "orders"."id" LIMIT \$2`).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg()).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "created_at", "updated_at", "deleted_at", "number", "status", "accrual", "user_id"}))
+
+	mock.ExpectBegin()
+	mock.ExpectExec(`UPDATE "users" SET "created_at"=\$1,"updated_at"=\$2,"deleted_at"=\$3,"email"=\$4,"password"=\$5,"balance"=\$6,"withdrawal"=\$7 WHERE "users"."deleted_at" IS NULL AND "id" = \$8`).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), 123).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO "balance_history" ("created_at","updated_at","deleted_at","user_id","amount","operation","order_id") VALUES ($1,$2,$3,$4,$5,$6,$7) ON CONFLICT ("id") DO UPDATE SET "user_id"="excluded"."user_id" RETURNING "id"`)).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+	mock.ExpectCommit()
+}
+
+func WithdrawHandlerMockQueryNoOrderError(mock sqlmock.Sqlmock) {
+	mock.ExpectQuery(`SELECT \* FROM "users" WHERE "users"."id" = \$1 AND "users"."deleted_at" IS NULL ORDER BY "users"."id" LIMIT \$2`).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg()).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "created_at", "updated_at", "deleted_at", "email", "password", "balance", "withdrawal"}).
+			AddRow(123, dateMock, dateMock, nil, "nUJ4D@example.com", "password", 5000000, 0))
+	mock.ExpectQuery(`SELECT \* FROM "orders" WHERE number = \$1 AND "orders"."deleted_at" IS NULL ORDER BY "orders"."id" LIMIT \$2`).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg()).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "created_at", "updated_at", "deleted_at", "number", "status", "accrual", "user_id"}))
+}
+
+func WithdrawHandlerMockQueryError(mock sqlmock.Sqlmock) {
+	mock.ExpectQuery(`SELECT \* FROM "users" WHERE "users"."id" = \$1 AND "users"."deleted_at" IS NULL ORDER BY "users"."id" LIMIT \$2`).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg()).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "created_at", "updated_at", "deleted_at", "email", "password", "balance", "withdrawal"}).
+			AddRow(123, dateMock, dateMock, nil, "nUJ4D@example.com", "password", 5000000, 0))
+	mock.ExpectQuery(`SELECT \* FROM "orders" WHERE number = \$1 AND "orders"."deleted_at" IS NULL ORDER BY "orders"."id" LIMIT \$2`).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg()).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "created_at", "updated_at", "deleted_at", "number", "status", "accrual", "user_id"}).
+			AddRow(1, dateMock, dateMock, nil, "order123", "PROCESSING", 1000, 123))
+
+	mock.ExpectBegin()
+	mock.ExpectExec(`UPDATE "users" SET "created_at"=\$1,"updated_at"=\$2,"deleted_at"=\$3,"email"=\$4,"password"=\$5,"balance"=\$6,"withdrawal"=\$7 WHERE "users"."deleted_at" IS NULL AND "id" = \$8`).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), 123).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO "balance_history" ("created_at","updated_at","deleted_at","user_id","amount","operation","order_id") VALUES ($1,$2,$3,$4,$5,$6,$7) ON CONFLICT ("id") DO UPDATE SET "user_id"="excluded"."user_id" RETURNING "id"`)).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+		WillReturnError(errors.New("some error"))
+	mock.ExpectRollback()
 }
 
 func upLogin(request *http.Request) *http.Request {
