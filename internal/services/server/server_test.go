7afc7ed90f06457d1e8e20966bd0bb504809373f
a.snegovskii <CarrionGrow@Gmail.com>
2024-06-09 12:20:23 +0300

    test

--- internal/services/server/server_test.go
+++ internal/services/server/server_test.go
@@ -1,7 +1,13 @@
 package server
 
 import (
+	"fmt"
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"net/http"
 	"testing"
 	"time"
 )
@@ -15,6 +21,75 @@ func TestServer_Run(t *testing.T) {
 		done <- s.Run("127.0.0.1:9090")
 	}()
 
+	select {
+	case err := <-done:
+		assert.Nil(t, err)
+	case <-time.After(1 * time.Second):
+		assert.NotNil(t, done)
+	}
+}
+
+func TestServer_APIRequests(t *testing.T) {
+	s, _ := NewServer("")
+	assert.NotNil(t, s)
+	db, _, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	gdb, _ := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+	gdb.Logger = gdb.Logger.LogMode(logger.Silent)
+
+	s.db = gdb
+	done := make(chan error)
+	go func() {
+		done <- s.Run("127.0.0.1:9090")
+	}()
+
+	time.Sleep(100 * time.Millisecond)
+	client := &http.Client{}
+
+	baseURL := "http://127.0.0.1:9090"
+	url := fmt.Sprintf("%s/api/user/register", baseURL)
+	Req, _ := http.NewRequest("POST", url, nil)
+	Resp, _ := client.Do(Req)
+	defer Resp.Body.Close()
+	assert.Equal(t, http.StatusBadRequest, Resp.StatusCode)
+
+	url = fmt.Sprintf("%s/api/user/login", baseURL)
+	Req, _ = http.NewRequest("POST", url, nil)
+	Resp, _ = client.Do(Req)
+	defer Resp.Body.Close()
+	assert.Equal(t, http.StatusBadRequest, Resp.StatusCode)
+
+	url = fmt.Sprintf("%s/api/user/orders", baseURL)
+	Req, _ = http.NewRequest("POST", url, nil)
+	Resp, _ = client.Do(Req)
+	defer Resp.Body.Close()
+	assert.Equal(t, http.StatusUnauthorized, Resp.StatusCode)
+
+	url = fmt.Sprintf("%s/api/user/balance", baseURL)
+	Req, _ = http.NewRequest("GET", url, nil)
+	Resp, _ = client.Do(Req)
+	defer Resp.Body.Close()
+	assert.Equal(t, http.StatusUnauthorized, Resp.StatusCode)
+
+	url = fmt.Sprintf("%s/api/user/balance/withdraw", baseURL)
+	Req, _ = http.NewRequest("POST", url, nil)
+	Resp, _ = client.Do(Req)
+	defer Resp.Body.Close()
+	assert.Equal(t, http.StatusUnauthorized, Resp.StatusCode)
+
+	url = fmt.Sprintf("%s/api/user/withdrawals", baseURL)
+	Req, _ = http.NewRequest("GET", url, nil)
+	Resp, _ = client.Do(Req)
+	defer Resp.Body.Close()
+	assert.Equal(t, http.StatusUnauthorized, Resp.StatusCode)
+
+	url = fmt.Sprintf("%s/api/user/orders", baseURL)
+	Req, _ = http.NewRequest("GET", url, nil)
+	Resp, _ = client.Do(Req)
+	defer Resp.Body.Close()
+	assert.Equal(t, http.StatusUnauthorized, Resp.StatusCode)
 	select {
 	case err := <-done:
 		assert.Nil(t, err)
